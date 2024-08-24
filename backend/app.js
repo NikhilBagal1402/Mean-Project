6e4a1f9bf5fbc7da591c20565793e6e0b1974b29
Nikhil Bagal <nikhil.bagal312@gmail.com>
2024-08-24 15:27:50 +0530

    minor-change

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use('/api/posts',(req,res,next) => {
-    const posts =[
-        
-    ]
-    res.json
+app.use("/api/posts", (req, res, next) => {
+  const posts = [
+    {
+      id: "dfgdsfg",
+      title: "coming from srver",
+      content: "Dummy post from server",
+    },
+  ];
+  res.json({
+    message: "post fetch successfully",
+  });
 });
 
 module.exports = app;
